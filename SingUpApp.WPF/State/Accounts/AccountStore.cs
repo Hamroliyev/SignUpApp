96cc3d11baad62ea0e77f4021cd9a1459476eff3
Kroos <hamroliyevahmadjon1909@gmail.com>
2023-10-12 17:08:17 +0500

    Add account strore .

---
+++ SingUpApp.WPF/State/Accounts/AccountStore.cs
@@ -0,0 +1,24 @@
+﻿using SignUpApp.Domain.Models;
+using System;
+
+namespace SignUpApp.WPF.State.Accounts
+{
+    public class AccountStore : IAccountStore
+    {
+        private Account _currentAccount;
+
+        public Account CurrentAccount
+        {
+            get { return _currentAccount; }
+            set
+            {
+                _currentAccount = value;
+                StateChanged?.Invoke();
+            }
+        }
+
+
+
+        public event Action StateChanged;
+    }
+}
