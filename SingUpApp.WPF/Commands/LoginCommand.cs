41b18e51b9ac7939f2488ed4fa89ce15ca40c31b
Kroos <hamroliyevahmadjon1909@gmail.com>
2023-10-12 17:07:31 +0500

    Add Commands and implement it.

---
+++ SingUpApp.WPF/Commands/LoginCommand.cs
@@ -0,0 +1,47 @@
+﻿using SignUpApp.WPF.State.Authenticators;
+using SignUpApp.WPF.State.Navigators;
+using SignUpApp.WPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignUpApp.WPF.Commands
+{
+    public class LoginCommand : AsyncCommandBase
+    {
+        private readonly LoginViewModel _loginViewModel;
+        private readonly IAuthenticator _authenticator;
+        private readonly IRenavigator _renavigator;
+
+        public LoginCommand(LoginViewModel loginViewModel, IAuthenticator authenticator, IRenavigator renavigator)
+        {
+            _loginViewModel = loginViewModel;
+            _authenticator = authenticator;
+            _renavigator = renavigator;
+        }
+
+        public override async Task ExecuteAsync(object parameter)
+        {
+            try
+            {
+                await _authenticator.Login(_loginViewModel.UserName, _loginViewModel.Password);
+
+                _renavigator.Renavigate();
+            }
+            catch (UserNotFoundException)
+            {
+                _loginViewModel.ErrorMessage = "UserName does not exist.";
+            }
+            catch (InvalidPasswordException)
+            {
+                _loginViewModel.ErrorMessage = "Incorrect password.";
+            }
+            catch (Exception)
+            {
+                _loginViewModel.ErrorMessage = "Login failed.";
+            }
+        }
+    }
+}
---
+++ SingUpApp.WPF/Commands/RegisterCommand.cs
@@ -0,0 +1,63 @@
+﻿using SignUpApp.Domain.Services.AuthenticationServices;
+using SignUpApp.WPF.State.Authenticators;
+using SignUpApp.WPF.State.Navigators;
+using SignUpApp.WPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignUpApp.WPF.Commands
+{
+    public class RegisterCommand : AsyncCommandBase
+    {
+        private readonly RegisterViewModel _registerViewModel;
+        private readonly IAuthenticator _authenticator;
+        private readonly IRenavigator _registerRenavigator;
+
+        public RegisterCommand(RegisterViewModel registerViewModel, IAuthenticator authenticator, IRenavigator registerRenavigator)
+        {
+            _registerViewModel = registerViewModel;
+            _authenticator = authenticator;
+            _registerRenavigator = registerRenavigator;
+        }
+
+        public override async Task ExecuteAsync(object parameter)
+        {
+            _registerViewModel.ErrorMessage = string.Empty;
+
+            try
+            {
+                RegistrationResult registrationResult = await _authenticator.Register(
+                       _registerViewModel.Email,
+                       _registerViewModel.Username,
+                       _registerViewModel.Password,
+                       _registerViewModel.ConfirmPassword);
+
+                switch (registrationResult)
+                {
+                    case RegistrationResult.Success:
+                        _registerRenavigator.Renavigate();
+                        break;
+                    case RegistrationResult.PasswordsDoNotMatch:
+                        _registerViewModel.ErrorMessage = "Password does not match confirm password.";
+                        break;
+                    case RegistrationResult.EmailAlreadyExists:
+                        _registerViewModel.ErrorMessage = "An account for this email already exists.";
+                        break;
+                    case RegistrationResult.UserNameAlreadyExists:
+                        _registerViewModel.ErrorMessage = "An account for this username already exists.";
+                        break;
+                    default:
+                        _registerViewModel.ErrorMessage = "Registration failed.";
+                        break;
+                }
+            }
+            catch (Exception)
+            {
+                _registerViewModel.ErrorMessage = "Registration failed.";
+            }
+        }
+    }
+}
